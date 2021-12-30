48123c990203f842b889e16ab8961b74cf5e8ea8
wide <1453648423@qq.com>
2021-12-30 17:16:25 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("My first page!")
+def play(request):
+    return HttpResponse("play game!")
