21e19994499ebdea1c9823c6e6ee76a612439bd8
wide <1453648423@qq.com>
2021-12-31 15:37:20 +0800

    game engine

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,8 +1,8 @@
 class AcGamePlayground{
     constructor(root){
         this.root = root;
-        this.$playground = $(`<div>playground</div>`);
-        this.hide();
+        this.$playground = $(`<div class="ac-game-playground">playground</div>`);
+        //this.hide();
         this.root.$ac_game.append(this.$playground);
         this.start();
     }
