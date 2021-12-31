10f5d555c4043628af16151b93e368a77334f49c
wide <1453648423@qq.com>
2021-12-31 09:46:49 +0800

    write html js css views urls

---
+++ game/urls/index.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from game.view.index import index
+
+urlpatterns = [
+    path("",index,name="index"),
+    path("menu/", include("game.urls.menu.index")),
+    path("background/", include("game.urls.background.index")),
+    path("settings/", include("game.urls.settings.index")),
+]
