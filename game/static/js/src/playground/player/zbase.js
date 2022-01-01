b2952b525674a7d1225ba4cfbc29295d9233f781
wide <1453648423@qq.com>
2022-01-01 17:09:56 +0800

    player moved

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -5,17 +5,53 @@ class Player extends AcGameObject{
         this.ctx = this.playground.game_map.ctx;
         this.x = x;
         this.y = y;
+        this.vx = 0;    //x方向移动比例
+        this.vy = 0;    //y方向移动比例
+        this.move_length = 0;
         this.radius = radius;
         this.color = color;
         this.speed = speed;
         this.is_me = is_me;
-        this.eps = 0.1;
+        this.eps = 0.1;     //小于这个值就算0
     }
 
     start(){
+        if(this.is_me){
+            this.add_listening_events();
+        }
     }
-
+    add_listening_events(){
+        let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu",function(){
+            return false;
+        });
+        this.playground.game_map.$canvas.mousedown(function(e){
+            if(e.which === 3){
+                outer.move_to(e.clientX, e.clientY);
+            }
+        });
+    }
+    get_dist(x1, y1, x2, y2){
+        let dx = x1 - x2;
+        let dy = y1 - y2;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    move_to(tx, ty){
+        this.move_length = this.get_dist(this.x, this.y, tx, ty);
+        let angle = Math.atan2(ty - this.y, tx - this.x);
+        this.vx = Math.cos(angle);
+        this.vy = Math.sin(angle);
+    }
     update(){
+        if(this.move_length < this.eps){
+            this.move_length = 0;
+            this.vx = this.xy = 0;
+        }else{
+            let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);     //真实移动距离
+            this.move_length -= moved;
+            this.x += this.vx * moved;
+            this.y += this.vy * moved;
+        }
         this.render();
     }
 
